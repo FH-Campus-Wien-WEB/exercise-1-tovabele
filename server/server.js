34f0b73e8815753b1aa9d4182009fc9a9dec8a8f
Tova Bele <tova.bele@stud.hcw.ac.at>
2026-03-18 12:46:50 +0100

    added exercise tasks as comments

--- server/server.js
+++ server/server.js
@@ -7,7 +7,11 @@ app.use(express.static(path.join(__dirname, 'files')));
 
 // Configure a 'get' endpoint for data..
 app.get('/movies', function (req, res) {
-  // Part 1: Remove the next line and replace with your code
+  /*/ Part 1:
+  Implement the server-side endpoint in server/server.js so that 
+  /movies returns an array of at least three movie objects in valid JSON, 
+  based on OMDb API example data (trimmed and reformatted as specified).
+  */
   res.send('!dlrow olleH')
 })
 
