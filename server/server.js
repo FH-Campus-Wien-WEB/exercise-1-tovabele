b0c05addfc96d03d1396036f71f7721bd244aa69
tova bele <tova.bele@stud.hcw.ac.at>
2026-03-22 19:14:14 +0100

    removed switch code for known key, added support for rogue keys

--- server/server.js
+++ server/server.js
@@ -30,17 +30,20 @@ const movies = [
         "imdbRating": 7.7
     },
     {
-        "Title": "Müllers Büro",
+        "Title": "Mï¿½llers Bï¿½ro",
         "Runtime": 104,
         "Released": "1986-06-19",
         "Genres": ["Comedy","Crime","Musical"],
         "Directors": ["Niki List","Hans Selikovsky"],
         "Writers": ["Niki List"],
-      "Actors": ["Andreas Vitásek","Barbara Rudnik","Christian Schmidt"],
-        "Plot": "Private detective Max Müller and his assistent Larry try to solve a crime but find themselves in strange bars and women.",
+      "Actors": ["Andreas Vitï¿½sek","Barbara Rudnik","Christian Schmidt"],
+        "Plot": "Private detective Max Mï¿½ller and his assistent Larry try to solve a crime but find themselves in strange bars and women.",
         "Poster": "https://m.media-amazon.com/images/M/MV5BMDg3MTIyNDItOGZiMi00NDE0LWI2NjEtNmExOGY0NmU2N2Q4XkEyXkFqcGdeQXVyNjEzMTc5NTQ@._V1_SX300.jpg",
         "Metascore": 100,
-        "imdbRating": 6.7
+        "imdbRating": 6.7,
+        "Language":"German",
+        "Country":"Austria",
+        "Awards":"N/A"
     }
 ]
 
@@ -70,9 +73,9 @@ app.get('/movies', function (req, res) {
   movie 2: hedwig
   http://www.omdbapi.com/?t=hedwig
   {"Title":"Hedwig and the Angry Inch","Year":"2001","Rated":"R","Released":"31 Aug 2001","Runtime":"95 min","Genre":"Comedy, Drama, Music","Director":"John Cameron Mitchell","Writer":"John Cameron Mitchell, Stephen Trask","Actors":"John Cameron Mitchell, Miriam Shor, Stephen Trask","Plot":"A gender-queer punk-rock singer from East Berlin tours the U.S. with her band as she tells her life story and follows the former lover/band-mate who stole her songs.","Language":"English, German","Country":"Canada, United States","Awards":"28 wins & 33 nominations total","Poster":"https://m.media-amazon.com/images/M/MV5BN2Q4MGJmYTgtNDhiMS00MTcwLTlhZjgtN2U5NWUyYzFiMWRmXkEyXkFqcGc@._V1_SX300.jpg","Ratings":[{"Source":"Internet Movie Database","Value":"7.7/10"},{"Source":"Rotten Tomatoes","Value":"92%"},{"Source":"Metacritic","Value":"85/100"}],"Metascore":"85","imdbRating":"7.7","imdbVotes":"38,305","imdbID":"tt0248845","Type":"movie","DVD":"N/A","BoxOffice":"$3,082,286","Production":"N/A","Website":"N/A","Response":"True"}
-  movie 3: Müllers Büro
+  movie 3: Mï¿½llers Bï¿½ro
   request: http://www.omdbapi.com/?t=m%C3%BCllers+b%C3%BCro
-  {"Title":"Müllers Büro","Year":"1986","Rated":"N/A","Released":"19 Jun 1986","Runtime":"104 min","Genre":"Comedy, Crime, Musical","Director":"Niki List, Hans Selikovsky","Writer":"Niki List","Actors":"Andreas Vitásek, Barbara Rudnik, Christian Schmidt","Plot":"Private detective Max Müller and his assistent Larry try to solve a crime but find themselves in strange bars and women.","Language":"German","Country":"Austria","Awards":"N/A","Poster":"https://m.media-amazon.com/images/M/MV5BMDg3MTIyNDItOGZiMi00NDE0LWI2NjEtNmExOGY0NmU2N2Q4XkEyXkFqcGdeQXVyNjEzMTc5NTQ@._V1_SX300.jpg","Ratings":[{"Source":"Internet Movie Database","Value":"6.7/10"}],"Metascore":"N/A","imdbRating":"6.7","imdbVotes":"810","imdbID":"tt0091594","Type":"movie","DVD":"10 Apr 2003","BoxOffice":"N/A","Production":"N/A","Website":"N/A","Response":"True"}
+  {"Title":"Mï¿½llers Bï¿½ro","Year":"1986","Rated":"N/A","Released":"19 Jun 1986","Runtime":"104 min","Genre":"Comedy, Crime, Musical","Director":"Niki List, Hans Selikovsky","Writer":"Niki List","Actors":"Andreas Vitï¿½sek, Barbara Rudnik, Christian Schmidt","Plot":"Private detective Max Mï¿½ller and his assistent Larry try to solve a crime but find themselves in strange bars and women.","Language":"German","Country":"Austria","Awards":"N/A","Poster":"https://m.media-amazon.com/images/M/MV5BMDg3MTIyNDItOGZiMi00NDE0LWI2NjEtNmExOGY0NmU2N2Q4XkEyXkFqcGdeQXVyNjEzMTc5NTQ@._V1_SX300.jpg","Ratings":[{"Source":"Internet Movie Database","Value":"6.7/10"}],"Metascore":"N/A","imdbRating":"6.7","imdbVotes":"810","imdbID":"tt0091594","Type":"movie","DVD":"10 Apr 2003","BoxOffice":"N/A","Production":"N/A","Website":"N/A","Response":"True"}
 
 
   Implement the server-side endpoint in server/server.js so that 
